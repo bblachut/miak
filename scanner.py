8ffae47b01740a16b6bd37ed5c3600bfb11fbde3
bblachut1 <bogojedi1@gmail.com>
2022-12-24 12:18:49 +0100

    update enum

--- scanner.py
+++ scanner.py
@@ -1,31 +1,50 @@
-from typing import Tuple
-from colorama import Fore
 from enum import Enum
+from typing import Tuple
 
 
 class Token(Enum):
-    PLUS = 1
-    MINUS = 2
-    TIMES = 3
-    DIV = 4
-    LPAREN = 5
-    RPAREN = 6
-    KEYWORD = 7
-    ID = 8
-    NUMBER = 9
-    ERROR = 10
-    BLANK = 11
-    EQUALS = 12
-    COLON = 13
+    ASSIGN = 1
+    IF_TOKEN = 2
+    ELSE_TOKEN = 3
+    WHILE_TOKEN = 4
+    FOR_TOKEN = 5
+    RETURN_TOKEN = 6
+    AND_TOKEN = 7
+    OR_TOKEN = 8
+    SKIP = 9
+    NOT_TOKEN = 10
+    CURLY_BRACKET_BEGIN = 11
+    CURLY_BRACKET_END = 12
+    ROUND_BRACKET_BEGIN = 13
+    ROUND_BRACKET_END = 14
+    SQUARE_BRACKET_BEGIN = 15
+    SQUARE_BRACKET_END = 16
+    QUOTATION_MARK = 17
+    COMPARISON_OPERATORS = 18
+    MATH_OPERATORS = 19
+    COMMA = 20
+    BETWEEN = 21
+    WHITESPACE = 22
+    NEW_LINE = 23
+    ERROR = 24
+    BOOLEAN = 25
+    ID = 26
+    NUMBER = 27
 
 
-keywords = {'if', 'else', 'elif', 'return', 'def', 'for', 'while'}
-token_colors = {Token.ERROR: Fore.RED, Token.KEYWORD: Fore.GREEN, Token.ID: Fore.YELLOW, Token.NUMBER: Fore.CYAN}
+keywords = {'if': Token.IF_TOKEN, 'or': Token.OR_TOKEN, 'for': Token.FOR_TOKEN, 'not': Token.NOT_TOKEN,
+            'else': Token.ELSE_TOKEN, 'while': Token.WHILE_TOKEN, 'return': Token.RETURN_TOKEN, 'continue': Token.SKIP,
+            'break': Token.SKIP, 'and': Token.AND_TOKEN, 'True': Token.BOOLEAN, 'False': Token.BOOLEAN}
 
 
 def scanner(text: str, i: int) -> Tuple[int, Token, str]:
-    if text[i] == '+':
+    if text[i] == '<':
+        if text[i + 1] == '=':
+            return i+1, Token.COMPARISON_OPERATORS, '<='
+        if text[i+1] == '-':
+            return i+1, Token.ASSIGN, '<-'
         return i, Token.PLUS, text[i]
+
     if text[i] == '-':
         return i, Token.PLUS, text[i]
     if text[i] == '*':
@@ -44,18 +63,18 @@ def scanner(text: str, i: int) -> Tuple[int, Token, str]:
         identifier = ''
         while i < len(text) and (text[i].isdigit() or text[i].isalpha()):
             identifier += text[i]
-            if identifier in keywords:
-                return i, Token.KEYWORD, identifier
+            if identifier in keywords.keys():
+                return i, keywords[identifier], identifier
             i += 1
-        return i-1, Token.ID, identifier
+        return i - 1, Token.ID, identifier
     if text[i].isdigit():
         number = ''
         while i < len(text) and text[i].isdigit():
             number += text[i]
             i += 1
         if i < len(text) and text[i].isalpha():
-            return i, Token.ERROR, number+text[i]
-        return i-1, Token.NUMBER, number
+            return i, Token.ERROR, number + text[i]
+        return i - 1, Token.NUMBER, number
     if text[i].isspace():
         return i, Token.BLANK, text[i]
     return i, Token.ERROR, text[i]
@@ -65,11 +84,7 @@ def scan(text: str) -> None:
     i = 0
     while i < len(text):
         i, token, communicat = scanner(text, i)
-        color = token_colors.get(token)
-        if color is not None:
-            print(color + communicat + Fore.RESET, end='')
-        else:
-            print(communicat + Fore.RESET, end='')
+        print(communicat, end='')
         i += 1
 
 
