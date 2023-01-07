06f6c9f9b0477f1e414096298968d64e3352012d
bdoncer <barbara.doncer01@gmail.com>
2023-01-07 11:39:02 +0100

    small changes in check statement

--- generator.py
+++ generator.py
@@ -24,7 +24,7 @@ class Generator:
     def _check_if_right_token(self, right_tokens: List[Token]) -> Tuple[Token, str]:
         token, communicat = self.next_token()
         if token not in right_tokens:
-            print("Segmentation fault")
+            print("Wrong token")
             exit()
         return token, communicat
 
@@ -60,8 +60,10 @@ class Generator:
             self.code += communicat
 
     def _check_for_statement(self) -> bool:
-        if self.saved_token != Token.FOR_TOKEN:
+        for_res = self._check_optional_token([Token.FOR_TOKEN])
+        if for_res[1] is None:
             return False
+
         try:  # chyba niepotrzebne
             id1 = self._check_if_right_token([Token.ID])
             ass1 = self._check_if_right_token([Token.ASSIGN])
@@ -69,7 +71,14 @@ class Generator:
             btw = self._check_if_right_token([Token.BETWEEN])
             id3 = self._check_if_right_token([Token.ID, Token.NUMBER])
 
-            self.code += f"for {id1[1]} in range({id2[1]},{id3[1]})"
+            self._add_to_code(*for_res)
+            self._add_to_code(*id1)
+            self._add_to_code(Token.ID, "in range")
+            self._add_to_code(Token.ROUND_BRACKET_BEGIN, "(")
+            self._add_to_code(*id2)
+            self._add_to_code(Token.ID, ",")
+            self._add_to_code(*id3)
+            self._add_to_code(Token.ROUND_BRACKET_END, ")")
 
             self._add_to_code(*self._check_if_right_token([Token.CURLY_BRACKET_BEGIN]))
 
@@ -81,7 +90,8 @@ class Generator:
         except EOFError:
             exit()
 
-    # BASIA
+        return True
+
     def _check_statement(self):
         if not self._check_for_statement():
             if not self._check_while_statement():
@@ -93,8 +103,9 @@ class Generator:
 
     def _check_expression(self):
         flag = True
-        if self._check_optional_token([Token.NOT_TOKEN]) != None:
-            self.code += "not "
+        not_res = self._check_optional_token([Token.NOT_TOKEN])
+        if not_res[1] is not None:
+            self._add_to_code(*not_res)
             flag = False
 
         if not self._check_math_expression():
@@ -112,10 +123,11 @@ class Generator:
 
 
     def _check_while_statement(self) -> bool:
-        if self.saved_token != Token.WHILE_TOKEN:
+        while_res = self._check_optional_token([Token.WHILE_TOKEN])
+        if while_res[1] is None:
             return False
         try:
-            self.code += "while"
+            self._add_to_code(*while_res)
             self._check_expression()
 
             self._add_to_code(*self._check_if_right_token([Token.CURLY_BRACKET_BEGIN]))
@@ -128,11 +140,14 @@ class Generator:
         except EOFError:
             exit()
 
+        return True
+
     def _check_if_statement(self) -> bool:
-        if self.saved_token != Token.IF_TOKEN:
+        if_res = self._check_optional_token([Token.IF_TOKEN])
+        if if_res[1] is None:
             return False
         try:
-            self.code += "if"
+            self._add_to_code(*if_res)
             self._check_expression()
             self._add_to_code(*self._check_if_right_token([Token.CURLY_BRACKET_BEGIN]))
             res = self._check_statement()
@@ -141,8 +156,9 @@ class Generator:
             self._add_to_code(*self._check_if_right_token([Token.CURLY_BRACKET_END]))
 
             # else
-            if self._check_optional_token([Token.ELSE_TOKEN]) != None:
-                self.code += "else"
+            else_res = self._check_optional_token([Token.ELSE_TOKEN])
+            if  else_res[1] is not None:
+                self._add_to_code(*else_res)
                 self._check_expression()
                 self._add_to_code(*self._check_if_right_token([Token.CURLY_BRACKET_BEGIN]))
 
@@ -154,28 +170,21 @@ class Generator:
         except EOFError:
             exit()
 
+        return True
+
     def _check_return_statement(self) -> bool:
-        if self.saved_token != Token.RETURN_TOKEN:
+        return_res = self._check_optional_token([Token.RETURN_TOKEN])
+        if return_res[1] is None:
             return False
-        self.code += "return"
+        self._add_to_code(*return_res)
         self._check_variable_type()
+
+        return True
 
     def _check_function_definition(self):
         pass
 
-    #merge
-    def _check_and_expression(self):
-        pass
-
-    def _check_or_expression(self):
-        pass
-
-    def _check_comparision_operators_expression(self):
-        pass
-
-    def _check_math_operators_expression(self):
-        pass
-
+    #and,or,comparision_operators,math_operators
     def _check_math_expression(self):
         pass
 
@@ -205,15 +214,4 @@ class Generator:
         pass
 
     def generate(self) -> str:
-
-        while (1):
-            token, communicat = self.next_token()
-            self.saved_token = token
-            if token == Token.FOR_TOKEN:
-                self._check_for_statement()
-            if token == Token.WHILE_TOKEN:
-                self._check_while_statement()
-            if token == Token.RETURN_TOKEN:
-                self._check_return_statement()
-            if token == Token.IF_TOKEN:
-                self._check_if_statement()
+        pass
